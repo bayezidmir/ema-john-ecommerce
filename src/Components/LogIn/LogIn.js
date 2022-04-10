c3c4d4a67b6a6f42c7bbe4d06915b539135757d7
bayezid mir <warner.roman@gmail.com>
2022-04-10 23:43:59 +0600

    fire base login applied

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,19 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import "./LogIn.css";
 
 const LogIn = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  if (user) {
+    navigate("/shop");
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(email, password);
+  };
+
   return (
     <div className="login-container">
       <p className="login-title">Login</p>
-      <form className="">
+      <form onSubmit={handleSubmit}>
         <div className="input-group">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="" required />
+          <input
+            onBlur={handleEmail}
+            type="email"
+            name="email"
+            id=""
+            required
+          />
         </div>
         <div className="input-group" style={{ marginTop: "10px" }}>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="" required />
+          <input
+            onBlur={handlePassword}
+            type="password"
+            name="password"
+            id=""
+            required
+          />
+          <p style={{ color: "red" }}>{error?.message}</p>
+          {loading && <p>loading...</p>}
         </div>
         <div className="submit-btn-container">
           <input type="submit" value="LogIn" className="btn-submit" />
